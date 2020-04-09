13449e5181fffa90873975d42673a80ddf87b8a7
victor.gutierrez <victor.gutierrez@pragma.com.co>
2020-04-09 10:58:05 -0500

    Add profiles settings

--- JavaProjects/src/main/java/springBoot/aws/configuration/DynamoDBConfig.java
+++ JavaProjects/src/main/java/springBoot/aws/configuration/DynamoDBConfig.java
@@ -2,23 +2,46 @@ package springBoot.aws.configuration;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.InstanceProfileCredentialsProvider;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class DynamoDBConfig {
 
+    @Value("${amazon.aws.accesskey:accesskey}")
+    private String accesskey;
+    @Value("${amazon.aws.secretkey:secretkey}")
+    private String secretkey;
+    @Value("${amazon.aws.region:region}")
+    private String region;
+
+    @Profile("dev")
     @Bean
     public DynamoDBMapper dynamoDBMapper() {
+        System.out.println("dev bean");
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("XXXXXX", "XXXXX")))
-                .withRegion(Regions.SA_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials(accesskey, secretkey)))
+                .withRegion(region)
                 .build();
         return new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
+    }
+
+    @Profile("qa")
+    @Bean
+    public DynamoDBMapper dynamoDBMapperQa() {
+        System.out.println("qa bean");
+        InstanceProfileCredentialsProvider provider = new InstanceProfileCredentialsProvider(false);
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(provider)
+                .build();
+        return new DynamoDBMapper(client);
     }
 }
